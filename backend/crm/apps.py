f50edeb7e53f0e38c8222e049a110f1a90321b72
Kamran Umar <82850563+kamranumar1002@users.noreply.github.com>
2026-06-22 04:09:08 +0500

    added Readme

--- backend/crm/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class CrmConfig(AppConfig):
-    name = 'crm'
-    
-    def ready(self):
-        """Import signals when app is ready"""
-        import crm.signals  # noqa: F401
--- backend/loans/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import LoanViewSet, PaymentViewSet
-
-router = DefaultRouter()
-router.register(r'loans', LoanViewSet, basename='loan')
-router.register(r'payments', PaymentViewSet, basename='payment')
-
-urlpatterns = router.urls
